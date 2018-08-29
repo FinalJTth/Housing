e7eecc114134806d1f8b29bf0f8413509dc6b108
ACER <ACER@LAPTOP-LJ59JI85>
2018-08-29 15:40:06 +0700

    Change the line of code inside DataJSON.java
    Enable tp write JSONArray inside the value of the storage specifically
    use in World.json

--- src/com/finaljtth/housing/DataJSON.java
+++ src/com/finaljtth/housing/DataJSON.java
@@ -27,6 +27,7 @@ public class DataJSON {
 	private static JSONParser parser = new JSONParser();
 	private static JSONObject json = new JSONObject();
 	
+	@SuppressWarnings("unchecked")
 	public static void setupJSON(Plugin plugin){
 		pluginFolder = plugin.getDataFolder().getAbsolutePath();
 		try {
@@ -36,8 +37,9 @@ public class DataJSON {
 	        if (!file.exists()) {
             	file.createNewFile();
             	FileWriter fileWriter = new FileWriter(file); 
-            	JSONObject empty = new JSONObject();
-            	fileWriter.write(rearrange(empty));
+            	JSONObject data = new JSONObject();
+            	data.put("type", "1");
+            	fileWriter.write(rearrange(data));
             	fileWriter.flush(); 
     	        fileWriter.close();
             }
@@ -47,15 +49,27 @@ public class DataJSON {
             	file.createNewFile();
             	FileWriter fileWriter = new FileWriter(file); 
             	JSONObject data = new JSONObject();
-            	JSONArray dataArray = new JSONArray();
+            	data.put("type", "2");
+            	fileWriter.write(rearrange(data));
+            	JSONObject group = new JSONObject();
+            	JSONArray grouplist = new JSONArray();
+            	JSONArray player = new JSONArray();
             	List<World> worldList = Bukkit.getWorlds();
             	for (int i=0; i < worldList.size(); i++) {
             		if (worldList.get(i).getName().contains("_housing")) {
             			String worldname = worldList.get(i).getName();
-            			dataArray
+            			group.put("Visitor", player);
+            			grouplist.add(group);
+            			group.put("Resident", player);
+            			grouplist.add(group);
+            			group.put("Co_owner", player);
+            			grouplist.add(group);
+            			group.put("Owner", player);
+            			grouplist.add(group);
+            			data.put(worldname, grouplist);
             		}
             	}
-            	fileWriter.write(rearrange(empty));
+            	fileWriter.write(rearrange(data));
             	fileWriter.flush(); 
     	        fileWriter.close();
             }
@@ -75,20 +89,17 @@ public class DataJSON {
 	}
 	
 	@SuppressWarnings("unchecked")
-	public static void writeJSON(String fileName, String subPath, String playername, String object, String value) {
-		try {
-			
+	public static boolean writeJSON(String fileName, String subPath, String playername, String object, String value) {
+		try {	
 			file = new File(pluginFolder + File.separator + subPath + fileName + ".json");
 	        filePath = new File(pluginFolder + File.separator + subPath);
 	        FileWriter fileWriter = new FileWriter(file); 
 	        filePath.mkdirs();
             if (!file.exists()) {
                 file.createNewFile();
-            	JSONObject empty = new JSONObject();
             	JSONObject data = new JSONObject();
-            	data.put("variable", "value");
-            	empty.put("playername", data);
-            	fileWriter.write(rearrange(empty));
+            	data.put("type", "1");
+            	fileWriter.write(rearrange(data));
             	fileWriter.flush(); 
     	        fileWriter.close();
             }
@@ -99,36 +110,85 @@ public class DataJSON {
             if(playerdata.get(playername) != null) {
             	dataArray = (JSONArray) playerdata.get(playername);
             }
+            else {
+            	playerdata.put("Type", "1");
+            	return false;
+            }
             System.out.println("JSON : " + playerdata.toJSONString());
             //JSONObject player = new JSONObject();
 		
 	        data.put(object, value);
 	        
 	        // Classified if the JSONArray of the player is empty or not
-	        if(!dataArray.isEmpty()) {
-	        	boolean exist = false;
-	        	int index = 0;
-	        	JSONArray tempArray = (JSONArray) playerdata.get(playername);	
-		        System.out.println("Array : " + tempArray.toJSONString());
-		        // Loop to check if the JSONArray of the JSONObject is exist or not
-		        for (int i = 0 ; i < tempArray.size(); i++) {
-		        	JSONObject tempJSONObject = (JSONObject) tempArray.get(i);
-		        	if(tempJSONObject.containsKey(object)){
-		        		exist = true;
-		        		index = i;
-		        		dataArray.set(i, data);
-		        		i = tempArray.size();
-		        	}
-		        }
-		        if(exist == true) {
-		        	dataArray.set(index, data);
+	        if(type == 1) {
+	        	if(!dataArray.isEmpty()) {
+		        	boolean exist = false;
+		        	int index = 0;
+		        	JSONArray tempArray = (JSONArray) playerdata.get(playername);	
+			        System.out.println("Array : " + tempArray.toJSONString());
+			        // Loop to check if the JSONArray of the JSONObject is exist or not
+			        for (int i = 0 ; i < tempArray.size(); i++) {
+			        	JSONObject tempJSONObject = (JSONObject) tempArray.get(i);
+			        	if(tempJSONObject.containsKey(object)){
+			        		exist = true;
+			        		index = i;
+			        		dataArray.set(i, data);
+			        		i = tempArray.size();
+			        	}
+			        }
+			        if(exist == true) {
+			        	dataArray.set(index, data);
+			        }
+			        else {
+			        	dataArray.add(data);
+			        }
 		        }
 		        else {
 		        	dataArray.add(data);
 		        }
 	        }
-	        else {
-	        	dataArray.add(data);
+	        else if (type == 2) {
+	        	if(!dataArray.isEmpty()) {
+		        	boolean exist = false;
+		        	int index = 0;
+		        	JSONArray tempArray = (JSONArray) playerdata.get(playername);	
+			        System.out.println("Array : " + tempArray.toJSONString());
+			        // Loop to check if the JSONArray of the JSONObject is exist or not
+			        for (int i = 0 ; i < tempArray.size(); i++) {
+			        	JSONObject tempJSONObject = (JSONObject) tempArray.get(i);
+			        	if(tempJSONObject.isEmpty()) {
+			        		JSONArray dataArray2 = new JSONArray();
+			        		dataArray2.add(value);
+				        	tempJSONObject.put(object, dataArray2);
+				        	dataArray.set(i, tempJSONObject);
+			        	}
+			        	else if(tempJSONObject.containsKey(object)){
+			        		JSONArray tempArray2 = (JSONArray) tempJSONObject.get(object);
+			        		for (int j = 0 ; j < tempArray2.size(); j++) {
+			        			String player = (String) tempArray2.get(i);
+			        			if(player == value){
+			        				exist = true;
+			        				j = tempArray2.size();
+			        			}
+			        		}
+			        		if(exist == true) {
+			        			return false;
+					        }
+			        		else {
+					        	tempArray2.add(value);
+					        	tempJSONObject.put(object, tempArray2);
+					        	dataArray.set(i, tempJSONObject);
+					        }
+			        	}
+			        }
+		        }
+		        else {
+		        	JSONObject group = new JSONObject();
+	            	JSONArray player = new JSONArray();
+	            	player.add(value);
+	            	group.put(object, player);
+		        	dataArray.add(group);
+		        }
 	        }
 	        
 	        
@@ -144,10 +204,12 @@ public class DataJSON {
 	        }
 	        fileWriter.flush(); 
 	        fileWriter.close(); 
+	        return true;
 	    }
 		catch (Exception ex) {
 			ex.printStackTrace();
 		}
+		return true;
 	}
 	
 	public static String readJSON(String fileName, String subPath, String playername, String object) {
